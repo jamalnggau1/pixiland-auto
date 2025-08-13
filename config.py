e757cf64d42833713a228b2262e4f5c692f1e298
Jamal nggau <44499007+jamalnggau1@users.noreply.github.com>
2025-08-13 17:06:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
-AUTHORIZATION = "Bearer TOKEN_KAMU"
-DELAY = 2 # Jeda antar request dalam detik
+# config.py
+
+# Ganti nilai di bawah ini dengan data dari browser Anda
+# Dapatkan data baru setiap kali akan memulai sesi bot
+
+USER_AGENT = "...... (isi dengan user agent Anda)"
+
+AUTHORIZATION_TOKEN = "Bearer ...... (isi dengan token otorisasi Anda)"
+
+# Menggunakan kurung agar string yang panjang lebih mudah dibaca
+COOKIE_STRING = (
+    "_ga=GA1.1.54195093.1754803624;...... (isi dengan string cookie Anda)")
